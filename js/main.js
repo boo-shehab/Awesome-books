376b83c6be22beea7c252c3ad30c53f9a2ffceee
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2022-10-24 16:42:12 +0300

    adding localStorage

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 const bookTitle = document.querySelector('#title');
 const author = document.querySelector('#author');
 const submitButton = document.querySelector('#submit');
-const container =document.querySelector('#books-container');
-
-BooksList = [];
+const container = document.querySelector('#books-container');
 
 
-submitButton.addEventListener('click', (e)=>{
-    e.preventDefault();
-    if(bookTitle.textContent ==='' && author.textContent === ''){
-        list = {title: bookTitle.textContent,
-                author: author.textContent   
-        };
-        BooksList.push(list);
-        console.log(bookTitle.innerHTML);
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (bookTitle.value.trim() !== '' && author.value.trim() !== '') {
+    const list = {
+      title: bookTitle.value,
+      author: author.value,
+    };
+    booksList.push(list);
+    setLocalStorage(booksList);
+    console.log(getLocalStorage());
+  }
+});
 
-    }
-})
+function setLocalStorage(booksList) {
+  localStorage.setItem('formInputs', JSON.stringify(booksList));
+}
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem('formInputs'));
+}
 
+const booksList = getLocalStorage();

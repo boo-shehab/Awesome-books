73393fe404024b21b20a12728ee1bee740c40a48
Daniel-Kigozi <kdanalex69@gmail.com>
2022-10-25 15:17:33 +0300

    add class to book-list-classes

--- js/main.js
+++ js/main.js
@@ -3,40 +3,53 @@ const author = document.querySelector('#author');
 const submitButton = document.querySelector('#submit');
 const container = document.querySelector('#books-container');
 
-function setLocalStorage(booksList) {
-  localStorage.setItem('formInputs', JSON.stringify(booksList));
-}
-function getLocalStorage() {
-  if (localStorage.getItem('formInputs') !== null) {
-    return JSON.parse(localStorage.getItem('formInputs'));
-  }
-  return [];
-}
-const booksList = getLocalStorage();
 
-function displaybooks(books) {
-  container.innerHTML = '';
-  for (let i = 0; i < books.length; i += 1) {
-    const book = books[i];
-    const maindiv = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const p = document.createElement('p');
-    const removeButton = document.createElement('button');
-    removeButton.addEventListener('click', () => {
-      booksList.splice(i, 1);
-      setLocalStorage(booksList);
-      displaybooks(booksList);
-    });
-    h2.innerHTML = book.title;
-    p.innerHTML = book.author;
-    removeButton.innerHTML = 'remove';
-    maindiv.appendChild(h2);
-    maindiv.appendChild(p);
-    maindiv.appendChild(removeButton);
-    container.appendChild(maindiv);
+class Books{
+  constructor(title, author){
+    this.title = title;
+    this.author = author;
   }
+
+  function setLocalStorage(booksList) {
+    localStorage.setItem('formInputs', JSON.stringify(booksList));
+  }
+
+  function getLocalStorage() {
+    if (localStorage.getItem('formInputs') !== null) {
+      return JSON.parse(localStorage.getItem('formInputs'));
+    }
+    return [];
+  }
+
+  function displaybooks(books) {
+    container.innerHTML = '';
+    for (let i = 0; i < books.length; i += 1) {
+      const book = books[i];
+      const maindiv = document.createElement('div');
+      const h2 = document.createElement('h2');
+      const p = document.createElement('p');
+      const removeButton = document.createElement('button');
+      removeButton.addEventListener('click', () => {
+        booksList.splice(i, 1);
+        setLocalStorage(booksList);
+        displaybooks(booksList);
+      });
+      h2.innerHTML = book.title;
+      p.innerHTML = book.author;
+      removeButton.innerHTML = 'remove';
+      maindiv.appendChild(h2);
+      maindiv.appendChild(p);
+      maindiv.appendChild(removeButton);
+      container.appendChild(maindiv);
+    }
+  }
+  
+
 }
+
+const booksList = getLocalStorage();
 displaybooks(booksList);
+
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
